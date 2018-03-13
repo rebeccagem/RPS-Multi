0122cb5a5783103972618507311babf633e2e27a
Rebecca Gemeinhardt <rebecca.geme@gmail.com>
2018-03-13 18:59:44 -0500

    player one stores in firebase and local storage

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,3 +1,99 @@
 $(document).ready(function () {
-console.log("Hi");
+
+    // Create a variable to reference the database.
+    var database = firebase.database();
+
+    var playerOne = {
+        name: " ",
+    };
+    var playerTwo = {
+        name: " ",
+    };
+
+
+//push players with player one and player two into the database
+
+      // Code for handling the push
+      database.ref().child("players").set({
+                playerOne: playerOne,
+                playerTwo: playerTwo
+              });
+
+
+
+
+//     // Using .on("value", function(snapshot)) syntax will retrieve the data
+//     // from the database (both initially and every time something changes)
+//     // This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
+//     database.ref().on("value", function(snapshot) {
+
+//         // Then we console.log the value of snapshot
+//         console.log(snapshot.val());
+
+//         // Then update the clickCounter variable with data from the database.
+//         playerOne = snapshot.val().players.playerOne;
+//         // playerTwo = snapshot.val().players.playerTwo;
+        
+//         console.log(playerOne);
+  
+//       // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+//       // Again we could have named errorObject anything we wanted.
+//       }, function(errorObject) {
+  
+//         // In case of error this will print the error
+//         console.log("The read failed: " + errorObject.code);
+//       });
+
+
+
+
+     // Capture Button Click
+     $("#userNameSubmit").on("click", function(event) {
+         // prevent form from trying to submit/refresh the page
+         event.preventDefault();
+
+         console.log("Player One: "+playerOne.name);
+         console.log("Player Two: "+playerTwo.name);
+
+//         if (playerOne.name === " "){
+         // Capture User Input and store them into variables
+         playerOne.name = $("#userNameInput").val().trim();
+         console.log(playerOne.name);
+         $("#userNameInputContainer").hide();
+
+         // Creat div to hold html welcome text
+         var welcomeDiv = $("<div>").addClass("welcomeDiv");
+         welcomeDiv.append("Hello, "+playerOne.name+". You are Player 1!");
+         $("#underTitle").append(welcomeDiv);
+    //  }
+//       // Code for handling the push
+       database.ref().child("players").set({
+         playerOne: playerOne,
+         playerTwo: playerTwo
+        });
+        ///LOCAL STORAGEEEEEE
+       localStorage.setItem("playerOneName", playerOne.name);
+
+// }
+//         else{
+//         console.log("There is already a player one");
+//         // Capture User Input and store them into variables
+//         playerTwo.name = $("#userNameInput").val().trim();
+//         console.log(playerTwo.name);
+//         $("#userNameInputContainer").hide();
+
+//         // Creat div to hold html welcome text
+//         var welcomeDiv = $("<div>").addClass("welcomeDiv");
+//         welcomeDiv.append("Hello, "+playerTwo.name+". You are Player 2!");
+//         $("#underTitle").append(welcomeDiv);
+
+//       // Code for handling the push
+//       database.ref().child("players").add({
+//         playerTwo: playerTwo,
+//       });
+      
+// }
+ });
+
+
 });
